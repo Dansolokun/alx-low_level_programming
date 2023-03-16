cbf91ec8d2be9e98af716af50a72fc3a1c3f93a5
Daniel Solomon <dsolomonkun@gmail.com>
2023-03-16 06:55:23 -0700

    Task size64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point "prints the size of various types on the computer it is compiled and run on."
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
