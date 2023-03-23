8eb7803456890f08325498221b5e1e0a92d2d6db
Daniel Solomon <dsolomonkun@gmail.com>
2023-03-23 16:55:26 -0700

    prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <math.h>
 
 /**
@@ -15,7 +14,7 @@ int main(void)
 	long x, maxf;
 	long number = 612852475143;
 	double square = sqrt(number);
-			
+
 	for (x = 1; x <= square; x++)
 	{
 	if (number % x == 0)
