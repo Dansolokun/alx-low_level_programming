214196c761676cea091751bb1a9d7e8399db64a7
Daniel Solomon <dsolomonkun@gmail.com>
2023-04-10 07:10:03 -0700

    Create the static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -10,15 +10,3 @@ char *_strncpy(char *dest, char *src, int n)
 
     return strncpy(dest, src, n);
 }
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-char *_strncpy(char *dest, char *src, int n)
-
-{
-
-    return strncpy(dest, src, n);
-}
